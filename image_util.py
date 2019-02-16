85a2d8d601aadd592d47328dde755c3369e38889
mark <markjava@foxmail.com>
2019-02-16 23:03:21 +0800

    拆分从12306下载图片方法

--- image_util.py
+++ image_util.py
@@ -11,26 +11,6 @@ requests.packages.urllib3.disable_warnings()
 imageDir = "temp/"
 curtImg = "curt1/"
 
-def downloadCaptcha(retry=False):
-    """
-    下载12306验证码图片
-    :param retry:
-    :return:
-    """
-    url = "https://kyfw.12306.cn/passport/captcha/captcha-image"
-    r = session.get(url, verify=False)
-    if retry:
-        urlBak = "https://kyfw.12306.cn/passport/captcha/captcha-check?answer=129%2C122%2C175%2C132&login_site=E&rand=sjrand"
-        time.sleep(3)
-        session.get(urlBak, verify=False)
-        time.sleep(3)
-        r = session.get(url, verify=False)
-    fileName = "temp/%s.png" % uuid.uuid4()
-    with open(fileName, "wb") as file:
-        file.write(r.content)
-    return fileName
-
-
 def judgeImageBackground(image):
     """
     判断验证码文字区域的词个数
