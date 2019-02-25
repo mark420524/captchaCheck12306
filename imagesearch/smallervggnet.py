b412dbfbf64bef5593f205bf1f83c228df474917
mark <markjava@foxmail.com>
2019-02-25 22:42:06 +0800

    add format lines

--- imagesearch/smallervggnet.py
+++ imagesearch/smallervggnet.py
@@ -13,43 +13,43 @@ class SmallerVGGNet:
     @staticmethod
     def build(width,height,depth,classes):
         # initialize the model along with the input shape to be
-		# "channels last" and the channels dimension itself
+        # "channels last" and the channels dimension itself
         model = Sequential()
-		inputShape = (height, width, depth)
-		chanDim = -1
+        inputShape = (height, width, depth)
+        chanDim = -1
         
-		# if we are using "channels first", update the input shape
-		# and channels dimension
-		if K.image_data_format() == "channels_first":
-			inputShape = (depth, height, width)
-			chanDim = 1
+        # if we are using "channels first", update the input shape
+        # and channels dimension
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
         # CONV => RELU => POOL
-		model.add(Conv2D(32, (3, 3), padding="same",
-			input_shape=inputShape))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(3, 3)))
-		model.add(Dropout(0.25))
+        model.add(Conv2D(32, (3, 3), padding="same",
+            input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(3, 3)))
+        model.add(Dropout(0.25))
 
         # (CONV => RELU) * 2 => POOL
-		model.add(Conv2D(64, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(64, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
         # (CONV => RELU) * 2 => POOL
-		model.add(Conv2D(128, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(Conv2D(128, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization(axis=chanDim))
-		model.add(MaxPooling2D(pool_size=(2, 2)))
-		model.add(Dropout(0.25))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
 
         # add 256 filter size 
@@ -74,15 +74,15 @@ class SmallerVGGNet:
 
 
         # first (and only) set of FC => RELU layers
-		model.add(Flatten())
-		model.add(Dense(1024))
-		model.add(Activation("relu"))
-		model.add(BatchNormalization())
-		model.add(Dropout(0.5))
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
  
-		# softmax classifier
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
  
-		# return the constructed network architecture
-		return model
+        # return the constructed network architecture
+        return model
